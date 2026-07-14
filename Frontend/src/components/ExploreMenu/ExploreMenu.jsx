821a3ed3974b6e3e1514f17881efe6ffb6dbfdb7
Labib-2025 <labib872006@gmail.com>
2026-07-14 07:39:02 +0600

    explore menu add

---
+++ Frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './ExploreMenu.css'
+import { menu_list } from '../../assets/assets'
+
+const ExploreMenu = () => {
+  return (
+    <div className='explore-menu' id='explore-menu'>
+        <h1>Explore our menu</h1>          
+        <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+        <div className="explore-menu-list">
+            {menu_list.map((iteam, index)=>{
+                return (
+                    <div key={index} className="explore-menu-list-item">
+                        <img src={iteam.menu_image} alt="" />
+                        <p>{iteam.menu_name}</p>
+                    </div>
+                )
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default ExploreMenu
