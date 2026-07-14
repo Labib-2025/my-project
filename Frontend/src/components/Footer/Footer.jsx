7338fc9c2540700f19ad5b1898db7e305b2bf9b7
Labib-2025 <labib872006@gmail.com>
2026-07-14 10:36:05 +0600

    footer add

---
+++ Frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+            <img src={assets.logo} alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident harum alias perferendis molestias sed, laborum nobis enim ipsa temporibus id eius nemo doloremque soluta veritatis suscipit nisi beatae inventore? Delectus.</p>
+            <div className="footer-social-icons">
+                <img src={assets.facebook_icon} alt="" />
+                <img src={assets.twitter_icon} alt="" />
+                <img src={assets.linkedin_icon} alt="" />
+            </div>
+        </div>
+        <div className="footer-content-center">
+            <h2>COMPANY</h2>
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+        <div className="footer-content-right">
+            <h2>GET IN TOUCH</h2>
+            <ul>
+                <li>+880-12345678</li>
+                <li>contact@tomato.com</li>
+            </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">Copyright 2026 &copy Tomato.com - All Right Reserved </p>
+    </div>
+  )
+}
+
+export default Footer
